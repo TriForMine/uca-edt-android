eb829fff7ec949cac987af846dea84e9310e2a1f
TriForMine <quentin@triformine.dev>
2022-09-12 12:23:29 +0200

    Initial commit

---
+++ app/src/main/java/dev/triformine/uca_edt/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.triformine.uca_edt.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
